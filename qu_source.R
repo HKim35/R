b9df4de1a49b59813b556d900eed7a8780859557
HKim35 <46041714+HKim35@users.noreply.github.com>
2019-03-11 17:36:54 +0900

    Add files via upload

---
+++ qu_source.R
@@ -0,0 +1,24 @@
+#-----------------------------------------------------------------------------
+source("https://raw.githubusercontent.com/statpnu/R/master/Rsam01.R")
+
+qu_source = function(data, p=0.5, conf=0.95, source = c("test", "statistic", "p.value", "noncentrality", "conf.int"))
+{
+	demand<-match.arg(source)
+	if (demand == "test"){
+	print(qt.test(data, p=p, con =conf))
+}
+	else if (demand == "statistic"){
+	print(as.numeric(qt.test(data, p=p, conf=conf)$statistic))
+}
+	else if (demand =="p.value"){
+	print(as.numeric(qt.test(data, p=p, conf=conf)$p.value))
+}
+	else if (demand =="noncentrality"){
+	print(as.numeric(qt.test(data, p=p, conf=conf)$parameter))
+}
+	else if (demand =="conf.int"){
+	print(as.numeric(qt.test(data, p=p, conf=conf)$conf.int))
+}
+}
+
+#----------------------------------------------------------------------------------
