319a687429bca648d10d3c70d5b32b2d8c536f22
HKim35 <46041714+HKim35@users.noreply.github.com>
2019-03-15 14:15:42 +0900

    Update qu_source.R

--- qu_source.R
+++ qu_source.R
@@ -1,23 +1,46 @@
 #-----------------------------------------------------------------------------
 source("https://raw.githubusercontent.com/statpnu/R/master/Rsam01.R")
 
-qt.source = function(data, p=0.5, conf=0.95, source = c("test", "statistic", "p.value", "noncentrality", "conf.int"))
+qt.source = function(x, p=0.5, conf=0.95, source=c("test", "statistic", "p.value", "noncentrality", "conf.int",
+				"sample.size","df","estimate","null.value","alternative","method","data.name"))
 {
 	demand<-match.arg(source)
+	DNAME = deparse(substitute(x))
 	if (demand == "test"){
 	print(qt.test(data, p=p, con =conf))
 }
 	else if (demand == "statistic"){
-	print(as.numeric(qt.test(data, p=p, conf=conf)$statistic))
+	print(as.numeric(qt.test(x, p=p, conf=conf)$statistic))
 }
-	else if (demand =="p.value"){
-	print(as.numeric(qt.test(data, p=p, conf=conf)$p.value))
+	else if (demand == "p.value"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$p.value))
 }
-	else if (demand =="noncentrality"){
-	print(as.numeric(qt.test(data, p=p, conf=conf)$parameter))
+	else if (demand == "noncentrality"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$parameter))
 }
-	else if (demand =="conf.int"){
-	print(as.numeric(qt.test(data, p=p, conf=conf)$conf.int))
+	else if (demand == "conf.int"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$conf.int))
+}
+	else if (demand == "sample.size"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$sample.size))
+}
+	else if (demand == "df"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$df))
+}
+	else if (demand == "estimate"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$estimate))
+}
+	else if (demand == "null.value"){
+	print(as.numeric(qt.test(x, p=p, conf=conf)$null.value))
+}
+	else if (demand == "alternative"){
+	print(as.character(qt.test(x, p=p, conf=conf)$alternative))
+}
+	else if (demand == "method"){
+	print(as.character(qt.test(x, p=p, conf=conf)$method))
+}
+	else if (demand == "data.name"){
+	print(DNAME)
 }
 }
 
